1cbf2fca5f6be6556aaa440199ad133615ecbeb1
Russell Simpkins <russellsimpkins@gmail.com>
2015-12-16 14:05:57 -0500

    inital checkin

---
+++ README.md
@@ -0,0 +1,35 @@
+=== Search Files
+
+The name might not be the best. This program lets you search for a file of strings for a file of strings.
+
+``` go run trie.go -i <input file> -s <search file> ```
+
+<input file> should be a list of strings to search through that are new line separated e.g.
+
+```
+/foo/bar/baz
+/some/other/one
+/and/yet/another
+this.is.another
+```
+
+The search file is also a list of strings to search the input file e.g.
+
+```
+this.is.another
+search/for/me
+/some/other/one
+```
+
+The program will print out each entry in <search file> with the match true false as a csv e.g.
+
+```
+this.is.another, true
+search/for/me, false
+/some/other/one, true
+```
+
+
+The code is a basic btrie data structure.
+
+TODO - add more tests.
---
+++ trie.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+// simple Trie stucture
+type Trie struct {
+	Next  map[uint8]Trie // next Tries, index on the int value of the char
+	Match bool           // ends will match
+}
+
+// test if the trie contains the string. Tests are recursive, starting with
+// the first char to at most len(string)
+func (trie *Trie) Contains(item string) (answer bool) {
+
+	items := len(item)
+	// if there's still more to the string, we can see if it's contained
+	if items > 1 {
+
+		// dig deeper if trie.Next is not nil
+		if t, ok := trie.Next[item[0]]; ok {
+			return t.Contains(item[1:])
+		}
+		// end of the line
+		return false
+	}
+
+	// end of the line, what say you?
+	return trie.Match
+}
+
+func NewTrie() (trie Trie) {
+	trie = Trie{}
+	trie.Next = make(map[uint8]Trie)
+	return trie
+}
+
+// add to the trie. Another recursive call to go from 0 ... len(item)
+func (trie *Trie) Add(item string) {
+
+	// if Next hasn't been initialized, do so
+	if trie.Next == nil {
+		trie.Next = make(map[uint8]Trie)
+	}
+
+	// see if were at the last char in the string
+	size := len(item)
+	if size > 1 {
+
+		// grab the first char in the input
+		idx := item[0]
+
+		// see if it's already in the map
+		if t, ok := trie.Next[idx]; ok {
+
+			// dig deeper
+			t.Add(item[1:])
+
+			// go idom, any changes to t need to be assigned since t is a copy
+			trie.Next[idx] = t
+			return
+		}
+
+		// it's not already in the map, create a new trie, initialize it's map
+		// and update our data structure
+		t := Trie{}
+		t.Next = make(map[uint8]Trie)
+
+		// dig deeper
+		t.Add(item[1:])
+
+		// again, go idiom
+		trie.Next[idx] = t
+		return
+	}
+
+	// we're at the end of the line, update match to true
+	trie.Match = true
+
+}
+
+func (trie *Trie) Load(filename string) (err error) {
+	thefile, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Unable to open %s read mode. %s\n", filename, err)
+		return
+	}
+	scanner := bufio.NewScanner(thefile)
+	for scanner.Scan() {
+		trie.Add(scanner.Text())
+	}
+	fmt.Printf("%v\n", trie)
+	thefile.Close()
+	return
+}
+
+func (trie *Trie) Check(filename string) (err error) {
+	thefile, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Unable to open %s read mode. %s\n", filename, err)
+		return
+	}
+	scanner := bufio.NewScanner(thefile)
+	for scanner.Scan() {
+		s := scanner.Text()
+		fmt.Printf("%s, %t\n", s, trie.Contains(s))
+	}
+	thefile.Close()
+	return
+}
+
+func main() {
+
+	inputfile := flag.String("i", "", "file that has strings you want to search")
+	testfile := flag.String("s", "", "filename that has strings you want to search for")
+	flag.Parse()
+
+	if *inputfile == "" {
+		fmt.Println("Need you to specify the abs filename of our input file -i")
+		return
+	}
+
+	if *testfile == "" {
+		fmt.Println("Need you to specify the abs filename of our test file -s")
+		return
+	}
+
+	t := Trie{}
+	t.Load(*inputfile)
+	//fmt.Printf("%v\n", t)
+	t.Check(*testfile)
+
+	if 1 == 1 {
+		return
+	}
+}
